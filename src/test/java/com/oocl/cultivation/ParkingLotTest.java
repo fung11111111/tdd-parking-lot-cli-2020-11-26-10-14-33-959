3c41848864d06ee9d08c84625133087ac3cedced
Ray Lam <ray.lam@oocl.com>
2020-11-27 22:02:00 +0800

    [ParkingBoy][Ray Lam] fix: code refracor - rename ParkingLotTest

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -6,14 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     @Test
-    void should_return_ticket_when_parkingCar_given_parkingBoy_parkingLot_with_availbale_capacity() {
+    void should_return_ticket_when_parkingCar_given_parkingLot_with_availbale_capacity() {
         //given
         ParkingLot parkinglot = new ParkingLot(1);
-        ParkingBoy parkingBoy = new ParkingBoy(parkinglot);
         Car car = new Car();
 
         //when
-        Ticket ticket = parkingBoy.park(car);
+        Ticket ticket = parkinglot.park(car);
 
         //then
         assertNotNull(ticket);
@@ -21,85 +20,76 @@ public class ParkingLotTest {
     }
 
     @Test
-    void should_return_noticket_when_parkingCar_given_parkingBoy_parkingLot_with_no_availbale_capacity() {
+    void should_return_noticket_when_parkingCar_given_parkingLot_with_no_availbale_capacity() {
         //given
         ParkingLot parkinglot = new ParkingLot(0);
-        ParkingBoy parkingBoy = new ParkingBoy(parkinglot);
         Car car = new Car();
 
         //when
-        Ticket ticket = parkingBoy.park(car);
+        Ticket ticket = parkinglot.park(car);
 
         //then
         assertNull(ticket);
     }
 
     @Test
-    void should_return_ticket_when_parkingCar_given_multicars_parkingBoy_parkingLot_with_availbale_capacity() {
+    void should_return_multi_tickets_when_parkingCar_given_multi_cars_parkingLot_with_availbale_capacity() {
         //given
         ParkingLot parkinglot = new ParkingLot(10);
-        ParkingBoy parkingBoy = new ParkingBoy(parkinglot);
         Car car1 = new Car();
         Car car2 = new Car();
 
         //when
-        Ticket ticket1 = parkingBoy.park(car1);
-        Ticket ticket2 = parkingBoy.park(car2);
+        Ticket ticket1 = parkinglot.park(car1);
+        Ticket ticket2 = parkinglot.park(car2);
 
         //then
         assertNotNull(ticket1);
         assertNotNull(ticket2);
-        assertNotEquals(ticket1,ticket2);
+        assertNotEquals(ticket1, ticket2);
     }
 
     @Test
-    void should_return_oneTicket_when_parkingCar_given_multicars_parkingBoy_parkingLot_with_1_availbale_capacity() {
+    void should_return_oneTicket_when_parkingCar_given_multicars_parkingLot_with_1_availbale_capacity() {
         //given
         ParkingLot parkinglot = new ParkingLot(1);
-        ParkingBoy parkingBoy = new ParkingBoy(parkinglot);
         Car car1 = new Car();
         Car car2 = new Car();
 
         //when
-        Ticket ticket1 = parkingBoy.park(car1);
-        Ticket ticket2 = parkingBoy.park(car2);
+        Ticket ticket1 = parkinglot.park(car1);
+        Ticket ticket2 = parkinglot.park(car2);
 
         //then
         assertNotNull(ticket1);
         assertNull(ticket2);
-
     }
 
-
     @Test
-    void should_return_car_when_fetchCar_given_parkinglot_has_car() {
+    void should_return_car_when_fetchCar_given_parkinglot_that_parked_car() {
         //given
         ParkingLot parkingLot = new ParkingLot(1);
-        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         Car car = new Car();
-        Ticket ticket = parkingBoy.park(car);
+        Ticket ticket = parkingLot.park(car);
 
         //when
         Car actual = parkingLot.fetchCar(ticket);
 
         //then
         assertEquals(car, actual);
-
     }
 
     @Test
-    void should_return_nocar_when_fetchCar_given_ticket_parkinglot_has_car() {
+    void should_return_nocar_when_fetchCar_given_ticket_parkinglot_that_parked_no_car() {
         //given
         ParkingLot parkingLot = new ParkingLot(1);
-        Car car = new Car();
-        Ticket ticket = parkingLot.park(car);
-        Ticket ticket1 = new Ticket();
+        Ticket ticket = new Ticket();
 
         //when
-        Car actual = parkingLot.fetchCar(ticket1);
+        Car car = parkingLot.fetchCar(ticket);
 
         //then
-        assertNotEquals(car, actual);
+        assertNull(car);
 
     }
 
