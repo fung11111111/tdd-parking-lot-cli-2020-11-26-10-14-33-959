b41f374dfedb8406e2b9231fa45386744cf52edf
Ray Lam <ray.lam@oocl.com>
2020-11-27 23:12:55 +0800

    [ParkingBoy 2][Ray Lam] test: implement throws notEnoughPositionException in return 1ticket given 2 cars and 1 capacity

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
-//    @Test
-//    void should_return_ticket_when_parkingCar_given_parkingLot_with_availbale_capacity() {
-//        //given
-//        ParkingLot parkinglot = new ParkingLot(1);
-//        Car car = new Car();
-//
-//        //when
-//        Ticket ticket = parkinglot.park(car);
-//
-//        //then
-//        assertNotNull(ticket);
-//
-//    }
+    @Test
+    void should_return_ticket_when_parkingCar_given_parkingLot_with_availbale_capacity() throws NotEnoughPosition{
+        //given
+        ParkingLot parkinglot = new ParkingLot(1);
+        Car car = new Car();
+
+        //when
+        Ticket ticket = parkinglot.park(car);
+
+        //then
+        assertNotNull(ticket);
+
+    }
 
     @Test
     void should_throw_notenoughposition_exception_when_parkingCar_given_parkingLot_with_no_availbale_capacity() throws NotEnoughPosition {
@@ -26,48 +26,49 @@ public class ParkingLotTest {
         Car car = new Car();
 
         //when
-        NotEnoughPosition notEnoughPositionexception = assertThrows(NotEnoughPosition.class, ()-> {
+        NotEnoughPosition notEnoughPositionException = assertThrows(NotEnoughPosition.class, () -> {
             parkinglot.park(car);
         });
 
-
         //then
-        assertEquals("Not enough position.", notEnoughPositionexception.getMessage());
+        assertEquals("Not enough position.", notEnoughPositionException.getMessage());
     }
 
-//    @Test
-//    void should_return_multi_tickets_when_parkingCar_given_multi_cars_parkingLot_with_10_availbale_capacity() {
-//        //given
-//        ParkingLot parkinglot = new ParkingLot(10);
-//        Car car1 = new Car();
-//        Car car2 = new Car();
-//
-//        //when
-//        Ticket ticket1 = parkinglot.park(car1);
-//        Ticket ticket2 = parkinglot.park(car2);
-//
-//        //then
-//        assertNotNull(ticket1);
-//        assertNotNull(ticket2);
-//        assertNotEquals(ticket1, ticket2);
-//    }
-//
-//    @Test
-//    void should_return_oneTicket_when_parkingCar_given_multicars_parkingLot_with_1_availbale_capacity() {
-//        //given
-//        ParkingLot parkinglot = new ParkingLot(1);
-//        Car car1 = new Car();
-//        Car car2 = new Car();
-//
-//        //when
-//        Ticket ticket1 = parkinglot.park(car1);
-//        Ticket ticket2 = parkinglot.park(car2);
-//
-//        //then
-//        assertNotNull(ticket1);
-//        assertNull(ticket2);
-//    }
-//
+    @Test
+    void should_return_multi_tickets_when_parkingCar_given_multi_cars_parkingLot_with_10_availbale_capacity() throws NotEnoughPosition{
+        //given
+        ParkingLot parkinglot = new ParkingLot(10);
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        //when
+        Ticket ticket1 = parkinglot.park(car1);
+        Ticket ticket2 = parkinglot.park(car2);
+
+        //then
+        assertNotNull(ticket1);
+        assertNotNull(ticket2);
+        assertNotEquals(ticket1, ticket2);
+    }
+
+    @Test
+    void should_return_oneTicket_and_throw_notenoughposition_when_parkingCar_given_multicars_parkingLot_with_1_availbale_capacity() throws NotEnoughPosition{
+        //given
+        ParkingLot parkinglot = new ParkingLot(1);
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        //when
+        Ticket ticket1 = parkinglot.park(car1);
+        NotEnoughPosition notEnoughPositionException = assertThrows(NotEnoughPosition.class, () -> {
+            Ticket ticket2 = parkinglot.park(car2);
+        });
+
+        //then
+        assertNotNull(ticket1);
+        assertEquals("Not enough position.", notEnoughPositionException.getMessage());
+    }
+
 //    @Test
 //    void should_return_car_when_fetchCar_given_parkinglot_that_parked_car() {
 //        //given
