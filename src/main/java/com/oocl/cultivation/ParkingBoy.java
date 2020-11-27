27f101396bca31daa9f91919ce3f697df34207a1
Ray Lam <ray.lam@oocl.com>
2020-11-27 15:02:56 +0800

    [ParkingBoy][Ray Lam] test: create test case of returning ticket

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,4 +1,11 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+    private ParkingLot parkinglot;
+    public ParkingBoy(ParkingLot parkinglot) {
+        this.parkinglot = parkinglot;
+    }
+    public Ticket park(Car car){
+        return null;
+    }
 }
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -1,10 +1,28 @@
 package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.time;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    void should_return_ticket_when_parkingCar_given_parkingBoy_parkingLot_with_availbale_capacity() {
+        //given 
+        ParkingLot parkinglot = Mockito.mock(ParkingLot.class);
+        ParkingBoy parkingBoy = new ParkingBoy(parkinglot);
+        Car car = new Car();
+        
+        //when 
+        parkingBoy.park(car);
 
+        //then
+        verify(parkinglot, times(1)).park();
+        
     }
+
+
+
 }
