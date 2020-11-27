14874f67075bcf87a8a9ce33ddc7cf15018b54f0
Ray Lam <ray.lam@oocl.com>
2020-11-28 01:10:21 +0800

    [ParkingBoy 3][Ray Lam] test: return ticket from parkinglot2 when first lot is full

--- src/test/java/com/oocl/cultivation/FooParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/FooParkingBoyTest.java
@@ -78,4 +78,30 @@ public class FooParkingBoyTest {
         assertEquals(parkinglot1, ticket1.getParkingLot());
 
     }
+    @Test
+    void should_return_ticket_from_parkingLot2_when_fooparkingboy_parkingCar_given_multicas_parkingLot1_with_no_capacity() throws NotEnoughPosition {
+        //given
+        ParkingLot parkinglot1 = new ParkingLot(0);
+        ParkingLot parkinglot2 = new ParkingLot(10);
+        ArrayList<ParkingLot> parkingLots = new ArrayList<ParkingLot>(){
+            {
+                add(parkinglot1);
+                add(parkinglot2);
+            }
+        };
+        FooParkingBoy fooParkingBoy = new FooParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        //when
+        Ticket ticket1 = fooParkingBoy.park(car1);
+        Ticket ticket2 = fooParkingBoy.park(car2);
+
+        //then
+        assertNotNull(ticket1);
+        assertNotNull(ticket2);
+        assertEquals(parkinglot2, ticket1.getParkingLot());
+
+    }
+
 }
