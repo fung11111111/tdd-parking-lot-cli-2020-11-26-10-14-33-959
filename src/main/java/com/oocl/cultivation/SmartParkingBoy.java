609c4a6e20c3fc9cb7bd17d0bdcf38d7716a838a
Ray Lam <ray.lam@oocl.com>
2020-11-28 12:59:18 +0800

    [ParkingBoy 4][Ray Lam] test: implement shd return ticket from parkinglot which has more space given multi parkinglots

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,15 @@
+package com.oocl.cultivation;
+
+import java.util.ArrayList;
+
+public class SmartParkingBoy {
+    private ArrayList<ParkingLot> parkingLots;
+
+    public SmartParkingBoy(ArrayList<ParkingLot> parkingLots){
+        this.parkingLots = parkingLots;
+    }
+
+    public Ticket park(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -0,0 +1,32 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.spy;
+
+public class SmartParkingBoyTest {
+    @Test
+    void should_return_ticket_from_parkinglot2_when_smartparkingboy_park_given_smartparkingboy_car_parkingLot2_with_more_capacity() throws NotEnoughPosition {
+        //given
+        ParkingLot parkinglot1 = new ParkingLot(4);
+        ParkingLot parkinglot2 = new ParkingLot(10);
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkinglot1);
+        parkingLots.add(parkinglot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+
+        //when
+        Ticket ticket = smartParkingBoy.park(car);
+
+        //then
+        assertNotNull(ticket);
+        assertEquals(parkinglot2,ticket.getParkingLot());
+
+    }
+}
